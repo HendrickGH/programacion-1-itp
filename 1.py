17d0683ea70fc027d5ae75c53fa78f2612d5ffb6
Hendrick <hendrick@nebulosastudio.com>
2024-10-20 23:06:22 -0600

    updating code

--- 1.py
+++ 1.py
@@ -16,60 +16,87 @@
 #Mostrar el promedio de los salarios calculados
 #Mostrar los trabajadores con ventas mayores a 20000
 
-trabajadores=0
-salarios=0
-m20k=0
-tprendas=0
-r="s"
-x="s"
-while r=="s":
-    trabajadores+=1
-    x= input ("¿El trabajador laboro horas extra? <s/n> ")
+trabajadores = 0
+salarios = 0
+m20k = 0
+tprendas = 0
+r = "s"
+x = "s"
+total_ventas = 0
+total_horas_extra = 0
+empleado_max_horas_extra = None
+max_horas_extra = 0
+total_dinero_horas_extra = 0
+salario_base = 2000
+
+while r == "s":
+    trabajadores += 1
+    nombre = input("Nombre del trabajador: ")
+
+    x = input("¿El trabajador laboro horas extra? <s/n> ")
     if x == "s":
         ext = int(input("¿Cuántas horas extra laboró? "))
         while ext < 1 or ext > 4:
             print("Ingrese las horas extra adecuadas (entre 1 y 4)")
             ext = int(input("¿Cuántas horas extra laboró? ")) 
+        total_horas_extra += ext
+        total_dinero_horas_extra += ext * 20  # Suma el dinero por horas extra
+        if ext > max_horas_extra:
+            max_horas_extra = ext
+            empleado_max_horas_extra = nombre
         ext = ext * 20
     else:
         ext = 0
 
-    prenda=int(input("¿Cuantas prendas vendio el trabajador? "))
-    while prenda<1 or prenda>200:
-        print ("Da un valor correcto de prendas (1 minimo, 200 maximo)")
-        prenda=int(input("¿Cuantas prendas vendio el trabajador? "))
-   
-    tprendas+=prenda
-    venta=prenda*200
-
-    if venta>10000 and venta <20000:
-        comis=venta*0.05
-    elif venta>20000:
-        comis=venta*0.07
-        m20k+=1
-    else:
-        comis=0
+    prenda = int(input("¿Cuántas prendas vendió el trabajador? "))
+    while prenda < 1 or prenda > 200:
+        print("Da un valor correcto de prendas (1 mínimo, 200 máximo)")
+        prenda = int(input("¿Cuántas prendas vendió el trabajador? "))
     
-    salbrut= 2000 + ext + comis 
-    
-    if salbrut <=3000:
-        impuesto= salbrut * 0.10
-        salnet= salbrut - impuesto - 100
-    elif salbrut >3000 and salbrut<=6000:
-        impuesto= salbrut * 0.15
-        salnet= salbrut - impuesto - 100
+    tprendas += prenda
+    venta = prenda * 200
+    total_ventas += venta
+
+    if venta > 10000 and venta < 20000:
+        comis = venta * 0.05
+    elif venta > 20000:
+        comis = venta * 0.07
+        m20k += 1
     else:
-        impuesto= salbrut * 0.20
-        salnet= salbrut - impuesto - 100
+        comis = 0
 
-    salarios+=salnet
-    print ("El salario bruto del empleado fue ", salbrut)
-    print ("El coste del seguro social es de $100MXN")
-    print ("El salario neto del trabajador fue ", salnet)
-    print ("El total de prendas vendidas por el trabajador fue de ", prenda)
-    r = input ("Desea realizar otro calculo <s/n> ")
+    salbrut = salario_base + ext + comis
 
-prom=salarios/trabajadores
-print ("El promedio de los salarios fue de ", prom) 
-print ("Trabajadores con ventas mayores a $20,000 ", m20k)   
-print ("Trabajadores procesados ", trabajadores)   
+    if salbrut <= 3000:
+        impuesto = salbrut * 0.10
+    elif salbrut > 3000 and salbrut <= 6000:
+        impuesto = salbrut * 0.15
+    else:
+        impuesto = salbrut * 0.20
+
+    salnet = salbrut - impuesto - 100  # Restar el seguro social
+    salarios += salnet
+
+    # Mensaje de por qué el salario subió si supera lo establecido
+    if salbrut > salario_base:
+        if venta > 10000:
+            print(f"El salario subió porque el trabajador vendió más de {venta} en prendas.")
+    
+    print(f"El salario bruto del empleado {nombre} fue {salbrut}")
+    print("El coste del seguro social es de $100MXN")
+    print(f"El salario neto del trabajador fue {salnet}")
+    print(f"El total de prendas vendidas por el trabajador fue de {prenda}")
+    r = input("¿Desea realizar otro cálculo <s/n>? ")
+
+prom = salarios / trabajadores
+prom_horas_extra = total_horas_extra / trabajadores if trabajadores > 0 else 0
+
+print(f"\nEl total de prendas vendidas por todos los empleados es {tprendas}")
+print(f"El total de ventas por todas las iteraciones es {total_ventas}")
+print(f"El promedio de salarios calculados es {prom}")
+print(f"El promedio de horas extra trabajadas por empleado es {prom_horas_extra}")
+print(f"El empleado que más horas extra trabajó fue {empleado_max_horas_extra} con {max_horas_extra} horas")
+print(f"El total de horas extra trabajadas fue {total_horas_extra}")
+print(f"El total de dinero en horas extra fue {total_dinero_horas_extra}")
+print(f"Trabajadores con ventas mayores a $20,000: {m20k}")
+print(f"Trabajadores procesados: {trabajadores}")
